5966d9fced1fa4186668cfd105fa61c45064b107
Craig Sparks <sparks.craig@gmail.com>
2013-04-10 10:40:08 -0400

    Allowing multiple configurations - Addresses #1

--- classes/kohana/elfinder.php
+++ classes/kohana/elfinder.php
@@ -7,29 +7,29 @@ class Kohana_elFinder {
 	public $config = NULL;
 	public $configJS = NULL;
 
-	private static $_instance = NULL;
+	private static $_instance = array();
 
 	private $_elFinder = NULL;
 
-	public function __construct()
+	public function __construct($config = 'default')
 	{
 		if (($path = Kohana::find_file('vendor', self::VENDOR_DIR . 'elFinder.init')))
 		{
 			ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(dirname($path)));
 			require_once self::VENDOR_DIR . 'elFinder.init.php';
 
-			$this->config = Kohana::$config->load('elfinder');
+			$this->config = Kohana::$config->load('elfinder.'.$config);
 			$this->configJS = Kohana::$config->load('elfinder-js');
 			$this->_elFinder = new elFinder($this->config);
 		}
 	}
 
-	public static function instance()
+	public static function instance($config = 'default')
 	{
-		if (!isset(self::$_instance))
-			self::$_instance = new Kohana_elFinder();
+		if (!isset(self::$_instance[$config]))
+			self::$_instance[$config] = new Kohana_elFinder($config);
 
-		return self::$_instance;
+		return self::$_instance[$config];
 	}
 
 	public function start($elementID = 'finder')
